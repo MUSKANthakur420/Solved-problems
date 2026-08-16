0f5f8018e162c01270f54bc1846a1523660cee0b
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-16 23:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (45.90%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int i, int j, int m, int n, vector<vector<int>>& triangle,
+              vector<vector<int>>& dp) {
+        if (i == m - 1) {
+            return triangle[i][j];
+        }
+        int row = 0, col = 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i + 1 < m)
+            row = triangle[i][j] + solve(i + 1, j, m, n, triangle, dp);
+        if (i + 1 < m && j + 1 < n)
+            col = triangle[i][j] + solve(i + 1, j + 1, m, n, triangle, dp);
+        return dp[i][j] = min(row, col);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        int n = triangle[m - 1].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        for (int j = 0; j < n; j++) {
+            dp[m - 1][j] = triangle[m - 1][j];
+        }
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = triangle[i].size() - 1; j >= 0; j--) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+};
