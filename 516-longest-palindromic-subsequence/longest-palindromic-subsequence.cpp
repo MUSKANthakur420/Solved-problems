803d745a3f1ad94f9df0cec7c70ace520d8dfe19
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-24 22:39:09 +0530

    Time: 75 ms (55.24%) | Memory: 75.1 MB (80.76%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    // int solve(int i, int j, strin a, string b) {
-    //     if (i >= a.size() || j >= b.size())
-    //         return 0;
-    //     if (a[i] == b[j]) {
-    //         return 1 + solve(i + 1, j + 1, a, b);
-    //     }
-    //     return max(solve(i, j + 1, a, b), solve(i + 1, j, a, b));
-    // }
+    int solve(int i, int j, string& s, string& rev) {
+        if (i >= s.size() || j >= s.size())
+            return 0;
+        if (s[i] == rev[j]) {
+            return 1 + solve(i + 1, j + 1, s, rev);
+        }
+        return max(solve(i, j + 1, s, rev), solve(i + 1, j, s, rev));
+    }
     int longestPalindromeSubseq(string s) {
-        string b = s;
-        reverse(b.begin(), b.end());
-        vector<int> dp(s.size() + 1);
+        string rev = s;
+        reverse(s.begin(), s.end());
+        vector<int> dp(s.size() + 1, 0);
         for (int i = s.size() - 1; i >= 0; i--) {
-            vector<int> curr(s.size() + 1);
+            vector<int> curr(s.size() + 1, 0);
             for (int j = s.size() - 1; j >= 0; j--) {
-                if (s[i] == b[j]) {
-                    curr[j] = 1 + dp[j + 1];
-                } else
+                if (s[i] == rev[j])
+                    curr[j] = dp[j + 1] + 1;
+                else
                     curr[j] = max(curr[j + 1], dp[j]);
             }
             dp = curr;
