989939e9af9c97b04bd7e753c40bcccdacb1ab66
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-26 22:41:42 +0530

    Time: 31 ms (53.43%) | Memory: 43.6 MB (42.52%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    map<string, int> mp;
-    int solve(int i, int j, string& a, string& b, vector<vector<int>>& dp) {
-        if (j == b.size())
+    int solve(int i, int j, string& s, string& t) {
+        if (j == t.size())
             return 1;
-        if (i == a.size() || j == b.size())
+        if (i >= s.size())
             return 0;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (a[i] == b[j]) {
-            return solve(i + 1, j + 1, a, b, dp) + solve(i + 1, j, a, b, dp);
-        } else
-            return solve(i + 1, j, a, b, dp);
-        return dp[i][j] = 0;
+        if (s[i] == t[j])
+            return solve(i + 1, j + 1, s, t) + solve(i + 1, j, s, t);
+        return solve(i + 1, j, s, t);
     }
-    int numDistinct(string a, string b) {
-        vector<long long int> dp(b.size() + 1, 0LL);
-        dp[b.size()] = 1;
-        for (int i = a.size() - 1; i >= 0; i--) {
-            vector<long long int> curr(b.size() + 1, 0LL);
-            curr[b.size()] = 1;
-            for (int j = b.size() - 1; j >= 0; j--) {
-                if (a[i] == b[j]) {
+    int numDistinct(string s, string t) {
+        vector<long long int> dp(t.size() + 1, 0LL);
+        dp[t.size()] = 1LL;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            vector<long long> curr(t.size() + 1, 0LL);
+            curr[t.size()] = 1LL;
+            for (int j = t.size() - 1; j >= 0; j--) {
+                if (s[i] == t[j]) {
                     curr[j] = (dp[j + 1] % INT_MAX + dp[j] % INT_MAX) % INT_MAX;
                 } else
                     curr[j] = dp[j];
