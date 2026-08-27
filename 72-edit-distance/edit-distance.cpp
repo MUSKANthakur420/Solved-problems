7a7e5c28c779ea80bdedb53a0f6fced5506bcc96
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-27 22:52:05 +0530

    Time: 9 ms (38.20%) | Memory: 12.5 MB (83.77%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -7,22 +7,21 @@ public:
             return t.size() - j;
         if (s[i] == t[j])
             return solve(i + 1, j + 1, s, t);
-        return min({1 + solve(i + 1, j, s, t), 1 + solve(i + 1, j + 1, s, t),
-                    1 + solve(i, j + 1, s, t)});
+        return min({1 + solve(i + 1, j + 1, s, t), 1 + solve(i + 1, j, s, t),
+                    1 + solve(i + 1, j + 1, s, t)});
     }
     int minDistance(string s, string t) {
-        int maxi = max(s.size(), t.size());
-        vector<int> dp(maxi + 1, 0);
-        for (int i = 0; i <= t.size(); i++)
-            dp[i] = t.size() - i;
+        vector<int> dp(t.size() + 1, 0);
+        for (int j = t.size(); j >= 0; j--)
+            dp[j] = t.size() - j;
         for (int i = s.size() - 1; i >= 0; i--) {
-            vector<int> curr(maxi + 1, 0);
+            vector<int> curr(t.size() + 1, 0);
             curr[t.size()] = s.size() - i;
             for (int j = t.size() - 1; j >= 0; j--) {
-                if (s[i] == t[j]) {
+                if (s[i] == t[j])
                     curr[j] = dp[j + 1];
-                } else
-                    curr[j] = min({1 + dp[j + 1], dp[j] + 1, curr[j + 1] + 1});
+                else
+                    curr[j] = 1 + min({dp[j + 1], curr[j + 1], dp[j]});
             }
             dp = curr;
         }
