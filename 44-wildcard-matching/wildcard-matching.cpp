d6075c6ddb702ee900e5a1e57b465623033e32d3
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-27 23:12:43 +0530

    Time: 63 ms (30.75%) | Memory: 52.3 MB (44.46%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
-    bool solve(int i, int j, string s, string t) {
+    bool solve(int i, int j, string& s, string& t) {
         if (j == t.size() && i == s.size())
             return true;
-        if (j == t.size())
-            return false;
         if (i == s.size()) {
             for (int k = j; k < t.size(); k++) {
                 if (t[k] != '*')
@@ -12,33 +10,33 @@ public:
             }
             return true;
         }
-        if (s[i] == t[j]) {
+        if (s[i] == t[j])
             return solve(i + 1, j + 1, s, t);
-        }
-        if (t[j] == '*') {
+        if (t[j] == '*')
             return solve(i, j + 1, s, t) || solve(i + 1, j, s, t);
-        }
         if (t[j] == '?')
             return solve(i + 1, j + 1, s, t);
         return false;
     }
-    bool isMatch(string s, string t) {
-        vector<bool> dp(t.size() + 1, 0);
-        dp[t.size()] = true;
-        for (int j = t.size() - 1; j >= 0; j--) {
-            if (t[j] == '*')
+    bool isMatch(string s, string p) {
+        vector<int> dp(p.size() + 1, 0);
+        dp[p.size()] = true;
+        for (int j = p.size() - 1; j >= 0; j--) {
+            if (p[j] == '*')
                 dp[j] = dp[j + 1];
             else
                 dp[j] = false;
         }
         for (int i = s.size() - 1; i >= 0; i--) {
-            vector<bool> curr(t.size() + 1, 0);
-            for (int j = t.size() - 1; j >= 0; j--) {
-                if (s[i] == t[j])
+            vector<int> curr(p.size() + 1, 0);
+            if (i == s.size())
+                curr[p.size()] = true;
+            for (int j = p.size() - 1; j >= 0; j--) {
+                if (s[i] == p[j])
                     curr[j] = dp[j + 1];
-                if (t[j] == '*')
+                if (p[j] == '*')
                     curr[j] = curr[j + 1] || dp[j];
-                if (t[j] == '?')
+                if (p[j] == '?')
                     curr[j] = dp[j + 1];
             }
             dp = curr;
