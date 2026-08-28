fd6303c21359fb9394b54577c29efb95af680e87
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-28 17:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (65.46%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,18 +17,16 @@ public:
         return dp[indx][flag] = max(take, notake);
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size() + 1, vector<int>(3, -1));
-        dp[prices.size()][1] = 0, dp[prices.size()][0] = 0;
+        int x0 = 0, x1 = 0;
         for (int i = prices.size() - 1; i >= 0; i--) {
             for (int j = 1; j >= 0; j--) {
                 int take = 0, notake = 0;
                 if (j) {
-                    take = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                    x1 = max(-prices[i] + x0, x1);
                 } else
-                    notake = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
-                dp[i][j] = max(take, notake);
+                    x0 = max(prices[i] + x1, x0);
             }
         }
-        return dp[0][1];
+        return x1;
     }
 };
