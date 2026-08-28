1c3c1cf69ca26be10fddf2f62c4b7854a1399646
MuskanThakur <420royalrajput50806@gmail.com>
2026-08-28 19:19:17 +0530

    Time: 1 ms (89.16%) | Memory: 79.1 MB (95.79%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int solve(int indx, vector<int>& prices, int flag,
-              vector<vector<vector<int>>>& dp, int t) {
-        if (indx == prices.size())
+    int solve(int indx, vector<int>& prices, vector<vector<int>>& dp, int t) {
+        if (indx == prices.size() || t == 4)
             return 0;
-        if (t == 2)
-            return 0;
+        if (dp[indx][t] != -1)
+            return dp[indx][t];
         int take = 0, notake = 0;
-        if (dp[indx][flag][t] != -1)
-            return dp[indx][flag][t];
-        if (flag) {
-            take = max(-prices[indx] + solve(indx + 1, prices, 0, dp, t),
-                       solve(indx + 1, prices, 1, dp, t));
+        if (t % 2 == 0) {
+            take = max(-prices[indx] + solve(indx + 1, prices, dp, t + 1),
+                       solve(indx + 1, prices, dp, t));
         } else {
-            notake = max(prices[indx] + solve(indx + 1, prices, 1, dp, t + 1),
-                         solve(indx + 1, prices, 0, dp, t));
+            notake = max(prices[indx] + solve(indx + 1, prices, dp, t + 1),
+                         solve(indx + 1, prices, dp, t));
         }
-        return dp[indx][flag][t] = max(take, notake);
+        return dp[indx][t] = max(take, notake);
     }
     int maxProfit(vector<int>& prices) {
-
-        vector<vector<int>> dp(2, vector<int>(3, 0));
+        vector<int> dp(5, 0);
         for (int i = prices.size() - 1; i >= 0; i--) {
-            for (int j = 1; j >= 0; j--) {
-                for (int t = 1; t >= 0; t--) {
-                    int take = 0, notake = 0;
-                    if (j) {
-                        take = max(-prices[i] + dp[0][t], dp[1][t]);
-                    } else
-                        notake = max(prices[i] + dp[1][t + 1], dp[0][t]);
-
-                    dp[j][t] = max(take, notake);
-                }
-                // dp = curr;
+            for (int t = 3; t >= 0; t--) {
+                int take = 0, notake = 0;
+                if (t % 2 == 0)
+                    take = max(-prices[i] + dp[t + 1], dp[t]);
+                else
+                    notake = max(prices[i] + dp[t + 1], dp[t]);
+                dp[t] = max(take, notake);
             }
         }
-        return max({dp[1][0], dp[1][1]});
+        return dp[0];
     }
 };
