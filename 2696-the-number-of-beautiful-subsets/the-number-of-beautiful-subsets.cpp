5a4480e6938c9b49910f3e5dac49e8b6b30c3fad
MuskanThakur <420royalrajput50806@gmail.com>
2026-09-19 23:51:32 +0530

    Time: 7 ms (97.70%) | Memory: 41.3 MB (29.29%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     int result = 0;
-    void solve(int indx, int k, vector<int>& nums, vector<int>& dp,
-               unordered_map<int, int>& mp) {
-        if (indx == nums.size()) {
-            result++;
-            return;
+    int solve(int indx, int k, int prev, vector<vector<int>>& dp,
+              vector<pair<int, int>>& mp) {
+        if (indx >= mp.size()) {
+            return 1;
         }
-        solve(indx + 1, k, nums, dp, mp);
-        if (mp.find(nums[indx] - k) == mp.end() &&
-            mp.find(nums[indx] + k) == mp.end()) {
-            mp[nums[indx]]++;
-            solve(indx + 1, k, nums, dp, mp);
-            mp[nums[indx]]--;
-            if (mp[nums[indx]] == 0)
-                mp.erase(nums[indx]);
+        if (dp[indx][prev] != -1)
+            return dp[indx][prev];
+        int notake = solve(indx + 1, k, 0, dp, mp);
+        int ways = (1 << mp[indx].second) - 1;
+        int x = mp[indx].first;
+        int take = 0;
+        if (!prev || indx == 0 || x - k != mp[indx - 1].first) {
+            take = ways * solve(indx + 1, k, 1, dp, mp);
         }
+        return dp[indx][prev] = take + notake;
     }
     int beautifulSubsets(vector<int>& nums, int k) {
-        vector<int> dp(nums.size() + 1, -1);
-        unordered_map<int, int> mp;
-        solve(0, k, nums, dp, mp);
-        return result - 1;
+        unordered_map<int, vector<int>> mp;
+        for (auto it : nums) {
+            mp[it % k].push_back(it);
+        }
+        int ans = 1;
+        for (auto [rem, v] : mp) {
+            sort(v.begin(), v.end());
+            vector<pair<int, int>> arr;
+            for (int x : v) {
+                if (arr.empty() || arr.back().first != x) {
+                    arr.push_back({x, 1});
+                } else
+                    arr.back().second++;
+            }
+            vector<vector<int>> dp(arr.size() + 1, vector<int>(2, -1));
+            ans *= solve(0, k, 0, dp, arr);
+        }
+        return ans - 1;
     }
 };
